2854e0a543aa7212252088dbfb0d47472d73caf5
FoxBigotry <152131156+FoxBigotry@users.noreply.github.com>
2024-07-03 18:34:33 +0700

    settings conflict

--- settings.py
+++ settings.py
@@ -10,6 +10,16 @@ class Settings(BaseSettings):
     PROXY: str
     MODEL_AI: str
 
+    # Default settings
+    PROMPT_TOKEN_PRICE: int = 5  # in usd
+    COMPLETION_TOKEN_PRICE: int = 15  # in usd
+    DEFAULT_MODEL: str = "gpt-4o-2024-05-13"
+    DEFAULT_TEMPERATURE: float = 0.8
+    HISTORY_LENGTH: int = 5  # in messages
+    AUDIOS_DIR: str = "tmp/audios/"
+    USER_IDS: str
+    ADMIN_ID: str | None = None
+
 
 settings = Settings()
 
